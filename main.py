f2ec6f12e646d941a022c2fe0572793957c49fa5
K1R1LLER <beenin@ya.ru>
2021-10-20 01:35:26 +0300

    test

--- main.py
+++ main.py
@@ -144,7 +144,6 @@ def go_home():
     bot.home()
     time.sleep(1)
     go_home_button.color = ui.WHITE
-    #status_timer = 0
     frame_number = 1
     current_status.update(bot)
     while current_status.status_obj.state == "Returning home":
@@ -160,10 +159,6 @@ def go_home():
             
         current_status.update(bot)
                     
-        #if status_timer == STATUS_UPDATE_TIMER * FPS:
-           # current_status.update(bot)
-            #status_timer = 0
-
 
         if (frame_number == FPS / 5):
             go_home_button.text = "returning home.  "
@@ -180,24 +175,14 @@ def go_home():
             frame_number = 0
 
         main_clock.tick(FPS)
-        #status_timer += 1
         frame_number += 1
 
     go_home_button.text = "go home"
     
     
-
-
-
 def clean_mode():
     #TODO
     pass
 
         
 main_menu()
-
-
-
-
-
-
